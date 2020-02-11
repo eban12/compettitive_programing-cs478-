d5f9ef565351ec50b148efa2744430dcd8458c8b
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-02-11 15:09:36 +0300

    Update splitArrayWithSameAverage.py

--- Competitive/DC Contest #7/splitArrayWithSameAverage.py
+++ Competitive/DC Contest #7/splitArrayWithSameAverage.py
@@ -2,17 +2,17 @@ class Solution:
     def splitArraySameAverage(self, A: List[int]) -> bool:
         dic = {}
         
-        def helper(i, B, C, sizeB, sizeC):
-            if (B % (100),C % (100),sizeB,sizeC) in dic:
-                return dic[(B % (100),C % (100),sizeB,sizeC)]
+        def helper(i, Bsum, Csum, sizeB, sizeC):
+            if (Bsum % (100),Csum % (100),sizeB,sizeC) in dic:
+                return dic[(Bsum % (100),Csum % (100),sizeB,sizeC)]
             
             if sizeB + sizeC >= len(A):
                 if sizeB == 0 or sizeC == 0:
                     return False
-                return (B  / sizeB) == (C / sizeC)  
+                return (Bsum  / sizeB) == (Csum / sizeC)  
         
-            ans = (helper(i + 1, B + A[i], C, sizeB+1, sizeC)) or (helper(i + 1, B, C + A[i], sizeB, sizeC + 1))
-            dic[(B % (100),C % (100),sizeB,sizeC)] = ans
+            ans = (helper(i + 1, Bsum + A[i], Csum, sizeB+1, sizeC)) or (helper(i + 1, Bsum, Csum + A[i], sizeB, sizeC + 1))
+            dic[(Bsum % (100),Csum % (100),sizeB,sizeC)] = ans
             return ans
         
         return helper(0,0,0,0,0)
