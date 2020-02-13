efc4440b5049aee9059641b4f1a4b7244f2c167f
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-02-13 13:54:12 +0300

    Create mergeTwoBinaryTrees.py

---
+++ Competitive/Day14/mergeTwoBinaryTrees.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 and not t2:return
+        
+        if t1 and not t2: return t1
+        
+        if t2 and not t1: return t2
+        
+        newRoot = TreeNode(t1.val + t2.val)
+        newRoot.left = self.mergeTrees(t1.left, t2.left)
+        newRoot.right = self.mergeTrees(t1.right, t2.right)
+        
+        return newRoot
+        
