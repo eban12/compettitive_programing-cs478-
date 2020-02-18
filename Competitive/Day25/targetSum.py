3de35ed440dd59a3c9ce376ea06c73e4179d149d
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-02-18 22:10:42 +0500

    Update targetSum.py

--- Competitive/Day25/targetSum.py
+++ Competitive/Day25/targetSum.py
@@ -1,20 +1,18 @@
-class Solution {
-    public int findTargetSumWays(int[] nums, int S) {
-        return helper(nums,0,0,S);
-    }
-    
-    public int helper(int[] lst, int idx, int t, int s){
-        if (idx == lst.length){
-            if (s == t){
-                return 1;
-            }else{
-                return 0;
-            }
-        }else{
-            int count = 0;
-            count += helper(lst,idx+1,t+lst[idx],s);
-            count += helper(lst,idx+1,t-lst[idx],s);
-            return count;
-        } 
-    }
-}
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        dictionary = {}
+        def helper(idx,t):
+            if not (idx,t) in dictionary:
+                if idx == len(nums):
+                    if S == t:
+                        return 1
+                    return 0
+                else:
+                    count = 0
+                    count += helper(idx+1,t + nums[idx])
+                    count += helper(idx+1,t-nums[idx])
+                    dictionary[(idx,t)] = count
+            return dictionary[(idx,t)]
+            
+        return helper(0,0)
+     
