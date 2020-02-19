041ff079848ad0d11a79f0e7a48415b339d5319c
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-02-19 14:38:46 +0300

    Update isBipartite.py

--- Competitive/Day25/isBipartite.py
+++ Competitive/Day25/isBipartite.py
@@ -5,30 +5,32 @@ class Solution:
         setA = set()
         setB = set()
         for i in range(len(graph)):
-            if i not in visited:
-                self.bfs(i,setA,setB,graph,visited)
-        
-        for i in setA:
-            if i in setB:
+            if i not in visited and not self.bfs(i,setA,setB,graph,visited):
                 return False
         return True
+        
     
     def bfs(self, start,setA,setB,graph,visited):
         visited.add(start)
         queue = collections.deque()
         queue.append((start,True))
         while queue:
-            pos, color = queue.popleft()
-            if color:
+            pos, Red = queue.popleft()
+            if Red and pos in setB:
+                return False
+            elif Red:
                 setA.add(pos)
+            elif not Red and pos in setA:
+                return False
             else:
                 setB.add(pos)
+                
             visited.add(pos)
             for j in graph[pos]:
                 if j not in visited:
-                    queue.append((j,not color))
+                    queue.append((j,not Red))
         
-        
+        return True
         
             
                 
