c2ef10d3968b69209eba10644894c29a3f34e637
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-09-01 06:10:56 +0300

    Update sparse_arrays.py

--- A2SV-2/Week-1/sparse_arrays.py
+++ A2SV-2/Week-1/sparse_arrays.py
@@ -1,1 +1,9 @@
-# solution 
+def matchingStrings(strings, queries):
+    count = {}
+    for string in strings:
+        if string in count:
+            count[string] += 1
+        else:
+            count[string] = 1
+    
+    return [count[query] if query in count else 0 for query in queries]
