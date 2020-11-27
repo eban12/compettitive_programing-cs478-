8dde29c643c22f30ac64c21c43a0aaac0d11610f
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-11-27 03:43:02 -0800

    Update Detect Cycles in 2D Grid.py

--- A2SV-2/Buddy-Contest/Detect Cycles in 2D Grid.py
+++ A2SV-2/Buddy-Contest/Detect Cycles in 2D Grid.py
@@ -1,19 +1,19 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
-        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if (i, j) not in visited:
-                    if self.dfs(grid, (i,j), (-1,-1), grid[i][j], visited):
+                if not grid[i][j].isupper():
+                    if self.dfs(grid, (i,j), (-1,-1), grid[i][j]):
                         return True
         return False
     
-    def dfs(self, grid, current, prev, curChar, visited):
-        if current in visited:
+    def dfs(self, grid, cur, prev, curChar):
+        x, y = cur
+        if grid[x][y].isupper():
             return True
         
-        visited.add(current)
-        for i, j in [(current[0]+1, current[1]), (current[0]-1, current[1]), (current[0], current[1]+1), (current[0], current[1]-1)]:
-            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and (i,j) != prev and grid[i][j] == curChar:
-                if self.dfs(grid, (i,j), current, curChar, visited):
+        grid[x][y] = grid[x][y].upper()
+        for i, j in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and (i,j) != prev and grid[i][j].lower() == curChar:
+                if self.dfs(grid, (i,j), cur, curChar):
                     return True
