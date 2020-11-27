5bc660eaad9ff48d64626330bb70fea26c0e5eaa
Eyob Alemu <it.eyob.alemu@gmail.com>
2020-11-27 05:26:08 -0800

    Update Minimum Numbers of Function Calls to Make Target Array.py

--- A2SV-2/Buddy-Contest/Minimum Numbers of Function Calls to Make Target Array.py
+++ A2SV-2/Buddy-Contest/Minimum Numbers of Function Calls to Make Target Array.py
@@ -1,18 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        operations = 0
-        while sum(nums) > 0:
-            divided = False
-            for i in range(len(nums)):
-                if nums[i] % 2 != 0:
-                    operations += 1
-                    nums[i] -= 1
-                
-                if nums[i] != 0:
-                    divided = True
-                    
-                nums[i] = nums[i] // 2
-                
-            if divided:
-                operations += 1
-        return operations
+        max_length = 0
+        count = 0
+        for num in nums:
+            binary_num = bin(num)
+            count += binary_num.count("1")
+            max_length = max(max_length, len(binary_num))
+        return count + max_length - 3
